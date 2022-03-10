4f6d836f46e749a321e478dece54677e819aa15f
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-03-10 09:06:34 +0000

    Added LocationSelector component

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import mapboxGl from 'mapbox-gl';
+//import { UberContext } from '../context/uberContext';
 
 const style = {
   wrapper: `flex-1 h-full w-full`,
 }
 
+//mapboxGl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+mapboxGl.accessToken = 'pk.eyJ1Ijoid2hpemtpZGVmb3MiLCJhIjoiY2t0bHJ2bXg5MW83ejJwbzY5anhueDVxbSJ9.rvXQ-7i9Z4aNs1kVvMD1tw';
+
 const Map = () => {
+  //const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext);
+
   useEffect(() => {
-    const map = new mapboxGl.Map({})
+    const map = new mapboxGl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
   }, []);
 
   return (
-    <div className={style.wrapper}>Map</div>
+    <div className={style.wrapper} id="map">Map</div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import LocationSelector from '../components/LocationSelector'
 import Map from '../components/Map'
 import Navbar from '../components/Navbar'
 
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 }
 
 export default function Home() {
@@ -20,6 +25,7 @@ export default function Home() {
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
           {/* Location selector */}
+          <LocationSelector />
           {/* Confirm ride */}
         </div>
       </div>
